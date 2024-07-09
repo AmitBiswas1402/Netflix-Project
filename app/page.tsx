f87bf8e0c3b565575ddbda221b3a6533320890d5
Amit Biswas <amit.142biswas@gmail.com>
2024-07-09 22:29:28 +0530

    sfb

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
-import { Button } from "@/components/ui/button";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./utils/auth";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const session = await getServerSession(authOptions)
+
+  if (!session) {
+    return redirect("/login")
+  } else {
+    return redirect('/home')
+  }
+
   return (
-    <div className="m-5">
-      <Button>Add</Button>
-      <h1>{session?.user?.image}</h1>
-      <h1>{session?.user?.email}</h1>
-      <h1>{session?.user?.name}</h1>
-    </div>
+    <></>
   );
 }

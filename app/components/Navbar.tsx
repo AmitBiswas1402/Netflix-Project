43122fd3b69b2c955c45925fd73484eb4c1c53a8
Amit Biswas <amit.142biswas@gmail.com>
2024-07-11 22:18:24 +0530

    Navbar almost done

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,28 @@
+'use client'
 import Image from "next/image";
 import Link from "next/link";
 import Logo from '../../public/netflix_logo.svg'
+import { usePathname } from "next/navigation";
+import { Bell, Search } from "lucide-react";
+import UserNav from "./UserNav";
+
+
+interface linkProps {
+name: string;
+href: string;
+}
+
+const links: linkProps[] = [
+{ name: "Home", href: "/home" },
+{ name: "TV Shows", href: "/home/shows" },
+{ name: "Movies", href: "/home/movies" },
+{ name: "Recently Added", href: "/home/recently" },
+{ name: "My List", href: "/home/user/list" },
+];
 
 export default function Navbar(){
+    const pathName = usePathname()
+
     return (
         <>
         <div className="w-full max-w-7xl mx-auto items-center justify-between px-5 sm:px-6 py-5 lg:px-8 flex">
@@ -11,8 +31,30 @@ export default function Navbar(){
                     <Image src={Logo} alt="Netflix logo" priority />
                 </Link>
                 <ul className="lg:flex gap-x-4 ml-14 hidden">
-                    
+                    {links.map((link,idx) => (
+                        <div key={idx}>
+                            {
+                               pathName === link.href ? (
+                                <li>
+                                    <Link href={link.href} className="text-white font-semibold underline text-sm">
+                                        {link.name}
+                                    </Link>
+                                </li>
+                               ) : (
+                                <Link href={link.href} className="text-gray-300 font-normal text-sm">                                   
+                                    {link.name}
+                                </Link>
+                               )
+                            }
+                        </div>
+                    ))}
                 </ul>
+            </div>
+
+            <div className="flex items-center gap-x-8">
+                <Search className="w-5 h-5 text-gray-300 cursor-pointer" />
+                <Bell className="w-5 h-5 text-gray-300 cursor-pointer" />
+                <UserNav />
             </div>
         </div>
         </>

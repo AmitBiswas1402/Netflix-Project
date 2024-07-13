a54f7285ad137e1caea43777c7ccbcacb010831b
Amit Biswas <amit.142biswas@gmail.com>
2024-07-13 22:34:59 +0530

    movie preview

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -5,15 +5,18 @@ import { redirect } from "next/navigation";
 import Navbar from "../components/Navbar";
 
 export default async function HomeLayout({children}: {children: ReactNode}) {
-    const session = await getServerSession(authOptions)
+    // const session = await getServerSession(authOptions)
 
-    if (!session) {
-        return redirect('/login')
-    }
+    // if (!session) {
+    //     return redirect('/login')
+    // }
 
     return (
         <div>
             <Navbar />
+            <main className='w-full max-w-7xl mx-auto sm:px-6'>
+                {children}
+            </main>
         </div>
     )
 }
